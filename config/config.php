998c98fcd8c98400b4ec03e63c2238b9e52570c4
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-08-25 13:49:33 -0300

    (feat) add cors config

--- config/config.php
+++ config/config.php
@@ -10,5 +10,12 @@ $dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
 $dotenv->load();
 new DatabaseConnection();
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+
+
 Auth::setSecretKey(getenv('SECRET_KEY'));
 Auth::setTokenExpiration(getenv('JWT_TIME'));
