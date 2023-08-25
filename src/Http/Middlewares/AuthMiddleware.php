ff067637e4ddaa5d69234c9e17daa3155b9d984e
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-08-25 13:49:05 -0300

    (refactor) rollback token exception

--- src/Http/Middlewares/AuthMiddleware.php
+++ src/Http/Middlewares/AuthMiddleware.php
@@ -2,24 +2,24 @@
 
 namespace MiniRest\Http\Middlewares;
 
+use Closure;
 use MiniRest\Exceptions\InvalidJWTToken;
-use MiniRest\Exceptions\Validations\TokenValidationException;
 use MiniRest\Http\Auth\Auth;
 use MiniRest\Http\Request\Request;
 use MiniRest\Http\Response\Response;
 
 class AuthMiddleware implements MiddlewareInterface
 {
-    public function handle(Request $request, $params, $next)
+    public function handle(Request $request, $next)
     {
         try {
             if (Auth::check($request)) {
-                return $next($request, $params);
+                return $next($request);
             } else {
                 Response::json(['error' => 'Acesso não autorizado'], 403);
             }
-        } catch (TokenValidationException $e) {
-            Response::json(['error' => $e->getMessage()], 403);
+        } catch (InvalidJWTToken $e) {
+            Response::json(['error' => $e->getMessage()], $e->getCode());
         }
     }
 }
