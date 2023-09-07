3e8d298ac94c74657cc0b22b6059ae72740f04d9
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-09-07 19:57:38 -0300

    (refactor): add errors method

--- src/Http/Request/RequestValidation/RequestValidator.php
+++ src/Http/Request/RequestValidation/RequestValidator.php
@@ -3,11 +3,14 @@
 namespace MiniRest\Http\Request\RequestValidation;
 
 use \Exception;
+use MiniRest\Helpers\StatusCode\StatusCode;
 use MiniRest\Http\Request\Request;
+use MiniRest\Http\Response\Response;
 
 class RequestValidator
 {
     protected $rules = [];
+    protected $errorMessages = [];
 
     /**
      * @throws \Exception
@@ -40,28 +43,41 @@ class RequestValidator
         return [$ruleName, $ruleParams];
     }
 
-    public function validate($data = null): array | bool
+    public function validate($data = null) : bool
     {
 
         if (!$data) $data = (new Request())->all()->get('json');
 
-        $errorMessages = [];
+        $this->errorMessages = [];
         foreach ($this->rules as $field => $rules) {
 
             if (!isset($data[$field])) {
-                $errorMessages[$field][] = "Parametro \"$field\" não encontrada.";
+                $this->errorMessages[$field][] = "Parametro \"$field\" não encontrada.";
                 continue;
             }
 
             foreach ($rules as $rule) {
                 if (!$rule['rule']->passes($data[$field], $rule['params'])) {
                     $errorMessage = $rule['rule']->errorMessage($field, $rule['params']);
-                    $errorMessages[$field][] = $errorMessage;
+                    $this->errorMessages[$field][] = $errorMessage;
                 }
             }
         }
 
-        if (count($errorMessages) > 0) return $errorMessages;
+        if (count($this->errorMessages) > 0) return false;
+
         return true;
+    }
+
+    public function errors(): void
+    {
+        if (!$this->validate() && count($this->errorMessages) > 0) {
+            $erro = [];
+            foreach ($this->errorMessages as $item){
+                $erro[] = $item[0];
+            }
+            Response::json(['errors' => $erro], StatusCode::REQUEST_ERROR);
+            return;
+        }
     }
 }
