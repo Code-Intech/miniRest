0042207b96a4d008c18b5cc31b932e56041c738d
Joao-Victor1 <s238672@gmail.com>
2023-10-01 16:21:07 -0300

    (feat),(fix)/skills,categories,professions

--- src/Http/Controllers/Categories/CategoriesController.php
+++ src/Http/Controllers/Categories/CategoriesController.php
@@ -2,15 +2,22 @@
 
 namespace MiniRest\Http\Controllers\Categories;
 
-use MiniRest\Models\Categories;
+use MiniRest\Repositories\CategoriesRepository;
 use MiniRest\Http\Response\Response;
 use MiniRest\Http\Controllers\Controller;
 
 class CategoriesController extends Controller
 {
+    private CategoriesRepository $categories;
+
+    public function __construct()
+    {
+        $this->categories = new CategoriesRepository();
+    }
+
     public function index()
     {
-        Response::json(['category' => $this->paginate(Categories::query())]);
+        Response::json(['category' => $this->categories->getAll()]);
     }
 }
 
