866f2eb5e59ac1e9d085b61c07c6feff94b9aaf6
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-10-18 21:32:59 -0300

    (fix): patch on allow methods

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+header("Access-Control-Allow-Methods: PUT, PATCH, POST, GET, OPTIONS, DELETE");
 
 
 Auth::setSecretKey(getenv('SECRET_KEY'));
