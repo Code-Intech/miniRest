4b45ac7d62592158e190599331c4c0027dab6d7b
Joao-Victor1 <s238672@gmail.com>
2023-10-21 20:24:26 -0300

    feat/create_servico

--- src/Http/Controllers/Servico/ServicoController.php
+++ src/Http/Controllers/Servico/ServicoController.php
@@ -3,12 +3,15 @@
 namespace MiniRest\Http\Controllers\Servico;
 
 use MiniRest\Actions\Servico\ServicoCreateAction;
+use MiniRest\DTO\AddressCreateDTO;
 use MiniRest\DTO\Servico\ServicoCreateDTO;
 use MiniRest\Exceptions\DatabaseInsertException;
 use MiniRest\Http\Controllers\Controller;
 use MiniRest\Http\Request\Request;
 use MiniRest\Http\Response\Response;
 use MiniRest\Http\Auth\Auth;
+use MiniRest\Models\Address;
+use MiniRest\Repositories\AddressRepository;
 use MiniRest\Repositories\ContratanteRepository;
 
 class ServicoController extends Controller
@@ -40,11 +43,15 @@ class ServicoController extends Controller
             $contratanteId = $contratanteRepository->storeContratante($userId);
         }
 
+        $enderecoRepository = new AddressRepository();
+        $enderecoDTO = new AddressCreateDTO($request);
+        $enderecoId = $enderecoRepository->store($enderecoDTO->toArray());
+
         $tb_contratante_idtb_contratante = $contratanteId;
 
         try {
             $servicoCreateAction = new ServicoCreateAction();
-            $servicoCreateAction->execute(new ServicoCreateDTO($request, $tb_contratante_idtb_contratante, $userId));
+            $servicoCreateAction->execute(new ServicoCreateDTO($request, $tb_contratante_idtb_contratante, $userId, $enderecoId));
             return Response::json(['message' => 'Serviço criado com sucesso'], 201);
 
         } catch (DatabaseInsertException $exception) {
