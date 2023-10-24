f31dacddb41f1c1fe15cf542cde2d58e919a2a85
Joao-Victor1 <s238672@gmail.com>
2023-10-24 16:58:28 -0300

    feat/servico_image

---
+++ src/Models/Servico/ServicoUploadImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MiniRest\Models\Servico;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServicoUploadImage extends model
+{
+    protected $table = "tb_img";
+    protected $fillable = [
+        "IMG",
+        "tb_servico_idtb_servico",
+        "tb_servico_tb_contratante_idtb_contratante",
+        "tb_servico_tb_contratante_tb_user_idtb_user",
+    ];
+}
+
+?>
